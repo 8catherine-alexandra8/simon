bf91f2643b528205d3f6333575a4d6aafd0b917a
Catherine Alexandra <8catherine.alexandra8@gmail.com>
2020-03-27 11:40:35 -0600

    added sound play and button flash to startGame fn

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+let buttons = [ $('#btn1'), $('#btn2'), $('#btn3'), $('#btn4') ];
+let buttonSounds = [
+	$('#sound1'),
+	$('#sound2'),
+	$('#sound3'),
+	$('#sound4')
+];
+
 $(document).keydown(function() {
 	startGame();
 });
 
 function startGame() {
-	console.log('game started');
+	let randomIndex = Math.floor(Math.random() * 4);
+	buttons[randomIndex].addClass('flash');
+	buttonSounds[randomIndex].trigger('play');
+	setTimeout(function() {
+		buttons[randomIndex].removeClass('flash');
+	}, 100);
 }
