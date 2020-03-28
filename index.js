3c4ab62985e58f72a802290e7f5259c81686fc7b
Catherine Alexandra <8catherine.alexandra8@gmail.com>
2020-03-28 13:15:02 -0600

    added event listener for user click response and play button sound fn

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ let buttonSounds = [
 	$('#sound3'),
 	$('#sound4')
 ];
+let simonPlayed = [];
+let userPlayed = [];
 
 $(document).keydown(function() {
 	startGame();
@@ -12,9 +14,45 @@ $(document).keydown(function() {
 
 function startGame() {
 	let randomIndex = Math.floor(Math.random() * 4);
-	buttons[randomIndex].addClass('flash');
+	startButton = buttons[randomIndex][0];
+	startButton.classList.add('flash');
+	simonPlayed.push(startButton.id);
 	buttonSounds[randomIndex].trigger('play');
 	setTimeout(function() {
-		buttons[randomIndex].removeClass('flash');
+		startButton.classList.remove('flash');
 	}, 100);
+}
+$(document).click('col', function(event) {
+	userClick = event.target;
+	userClick.classList.add('flash');
+	playButtonSound(event.target.id);
+	setTimeout(function() {
+		userClick.classList.remove('flash');
+	}, 100);
+	userPlayed.push(event.target.id);
+});
+
+function playButtonSound(id) {
+	switch (id) {
+		case 'btn1':
+			const clip1 = $('#sound1');
+			clip1.trigger('play');
+			break;
+		case 'btn2':
+			const clip2 = $('#sound2');
+			clip2.trigger('play');
+			break;
+		case 'btn3':
+			const clip3 = $('#sound3');
+			clip3.trigger('play');
+			break;
+		case 'btn4':
+			const clip4 = $('#sound4');
+			clip4.trigger('play');
+			break;
+
+		default:
+			console.log(id);
+			break;
+	}
 }
