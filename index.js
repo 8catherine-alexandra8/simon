1838ae89395f69aeadc06cd2eae4cc5e7ae2fbaf
Catherine Alexandra <8catherine.alexandra8@gmail.com>
2020-03-29 09:07:29 -0600

    trying to figure out what I need to pass between the click listener and the userClickResponse function to get it to work without an uncaught error

--- index.js
+++ index.js
@@ -18,22 +18,28 @@ function startGame() {
 	startButton = buttons[randomIndex][0];
 	startButton.classList.add('flash');
 	simonPlayed.push(startButton.id);
+	console.log(`simonPlayed array = ${simonPlayed}`);
 	buttonSounds[randomIndex].trigger('play');
 	setTimeout(function() {
 		startButton.classList.remove('flash');
 	}, 100);
-	userClickResponse();
 }
-function userClickResponse() {
-	$(document).click('col', function(event) {
-		userClick = event.target;
-		userClick.classList.add('flash');
-		playButtonSound(event.target.id);
-		setTimeout(function() {
-			userClick.classList.remove('flash');
-		}, 100);
-		userPlayed.push(event.target.id);
-	});
+$(document).click('col', function(event) {
+	userClickResponse(event.target);
+});
+
+function userClickResponse(target) {
+	userClick = event.target;
+	userClick.classList.add('flash');
+	playButtonSound(event.target.id);
+	setTimeout(function() {
+		userClick.classList.remove('flash');
+	}, 100);
+	userPlayed.push(event.target.id);
+	console.log(`userPlayed array = ${userPlayed}`);
+	setTimeout(function() {
+		simonsTurn();
+	}, 2000);
 }
 
 function playButtonSound(id) {
@@ -59,4 +65,16 @@ function playButtonSound(id) {
 			console.log(id);
 			break;
 	}
+}
+function simonsTurn() {
+	let index = Math.floor(Math.random() * 4);
+	nextPlay = buttons[index][0];
+	nextPlay.classList.add('flash');
+	simonPlayed.push(nextPlay.id);
+	console.log(`simonPlayed array = ${simonPlayed}`);
+	buttonSounds[index].trigger('play');
+	setTimeout(function() {
+		nextPlay.classList.remove('flash');
+	}, 100);
+	userClickResponse();
 }
