34c07a80f74187c06a6736dd035a3bb5b7255724
Catherine Alexandra <8catherine.alexandra8@gmail.com>
2020-03-31 09:15:15 -0600

    added timeout to comparePlays fn so that only final userPlayed value used for comparison

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let buttonSounds = [
 ];
 let simonPlayed;
 let userFirstPlay;
-let userPlayed = [];
+let userPlayed = 'a';
 let clickCount = 0;
 
 $(document).keydown(function() {
@@ -24,20 +24,36 @@ function startGame() {
 	buttonSounds[randomIndex].trigger('play');
 	setTimeout(function() {
 		startButton.classList.remove('flash');
-	}, 100);
+	}, 50);
 }
+function simonsTurn() {
+	let index = Math.floor(Math.random() * 4);
+	let nextPlay = buttons[index][0];
+	nextPlay.classList.add('flash');
+	simonPlayed = simonPlayed + nextPlay.id;
+	console.log(`after simonsTurn, simonPlayed  = ${simonPlayed}`);
+	buttonSounds[index].trigger('play');
+	setTimeout(function() {
+		nextPlay.classList.remove('flash');
+	}, 50);
+	userPlayed;
+	clickCount = 1;
+	console.log(userPlayed);
+}
+
 $(document).click('col', function(event) {
 	clickCount++;
 	console.log(clickCount);
 	if (clickCount === 1) {
 		userFirstTurn(event.target);
+		buttonSoundAndFlash(event.target.id);
 	} else {
 		console.log(clickCount);
 		userTurn(event.target);
+		buttonSoundAndFlash(event.target.id);
 	}
 });
 function userFirstTurn(target) {
-	playButtonSound(event.target.id);
 	userFirstPlay = event.target.id;
 	console.log(
 		`userFirstTurn with clickCount of ${clickCount} and userFirstPlay value of ${userFirstPlay}`
@@ -46,14 +62,30 @@ function userFirstTurn(target) {
 }
 
 function userTurn(target) {
-	playButtonSound(event.target.id);
-
+	userPlay = event.target.id;
+	userPlayed = userPlayed + userPlay;
 	console.log(userPlayed);
-	userPlayed.push(event.target.id);
-	console.log(userPlayed);
+	comparePlays(userPlayed);
 }
 
-function playButtonSound(id) {
+function comparePlays() {
+	setTimeout(function() {
+		console.log(userPlayed);
+		if (
+			simonPlayed === userFirstPlay ||
+			'a' + simonPlayed === userPlayed
+		) {
+			console.log(
+				`in comparePlays, simonPlayed is ${simonPlayed}, userFirstPlay is ${userFirstPlay} and userPlayed is ${userPlayed}`
+			);
+			userPlayed = 'a';
+			simonsTurn();
+		} else {
+			return;
+		}
+	}, 3000);
+}
+function buttonSoundAndFlash(id) {
 	switch (id) {
 		case 'btn1':
 			const clip1 = $('#sound1');
@@ -62,7 +94,7 @@ function playButtonSound(id) {
 			$('#btn1').addClass('flash');
 			setTimeout(function() {
 				$('#btn1').removeClass('flash');
-			}, 100);
+			}, 50);
 			break;
 		case 'btn2':
 			const clip2 = $('#sound2');
@@ -71,7 +103,7 @@ function playButtonSound(id) {
 			$('#btn2').addClass('flash');
 			setTimeout(function() {
 				$('#btn2').removeClass('flash');
-			}, 100);
+			}, 50);
 			break;
 		case 'btn3':
 			const clip3 = $('#sound3');
@@ -80,7 +112,7 @@ function playButtonSound(id) {
 			$('#btn3').addClass('flash');
 			setTimeout(function() {
 				$('#btn3').removeClass('flash');
-			}, 100);
+			}, 50);
 			break;
 		case 'btn4':
 			const clip4 = $('#sound4');
@@ -89,37 +121,10 @@ function playButtonSound(id) {
 			$('#btn4').addClass('flash');
 			setTimeout(function() {
 				$('#btn4').removeClass('flash');
-			}, 100);
+			}, 50);
 			break;
 		default:
 			console.log(id);
 			break;
-	}
-}
-function simonsTurn() {
-	let index = Math.floor(Math.random() * 4);
-	nextPlay = buttons[index][0];
-	nextPlay.classList.add('flash');
-	simonPlayed = simonPlayed + nextPlay.id;
-	console.log(`after simonsTurn, simonPlayed  = ${simonPlayed}`);
-	buttonSounds[index].trigger('play');
-	setTimeout(function() {
-		nextPlay.classList.remove('flash');
-	}, 100);
-	userPlayed = [];
-	clickCount = 1;
-	console.log(userPlayed);
-}
-
-function comparePlays(userFirstPlay, userPlayed) {
-	if (simonPlayed === userFirstPlay || simonPlayed === userPlayed) {
-		console.log(
-			`in comparePlays, simonPlayed is ${simonPlayed}, userFirstPlay is ${userFirstPlay} and userPlayed is ${userPlayed}`
-		);
-		setTimeout(function() {
-			simonsTurn();
-		}, 2000);
-	} else {
-		return;
 	}
 }
